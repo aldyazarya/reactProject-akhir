7b68830d9b90ac374f3df69a4487fe6b2964e6f0
aldyazarya <aldy1612@gmail.com>
2019-04-11 16:30:02 +0700

    action edited

--- src/action/index.js
+++ src/action/index.js
@@ -4,88 +4,45 @@ import cookies from 'universal-cookie'
 
 const cookie = new cookies()
 
-export const onLoginClick = (user, pass) => {
-    return (dispatch) => {
-        // cek di database untuk username dan password yang di input
-        axios.get("http://localhost:1996/users", {
-            params: {
-                username: user,
-                password: pass
-            }
-        }).then(res => { 
-            // jika data yang dicari ditemukan, maka res.data.length > 0
-            if (res.data.length > 0) {
 
-                const {id, username} = res.data[0]
+export const onLogin = (email, password) => {
+    return async dispatch => {
+        try {
+            const res = await axios.post('/users/login', {email, password})
+            console.log(res);
+            
+            cookie.set('masihLogin', res.data.name, {path:'/'})
+            cookie.set('idLogin', res.data._id, {path:'/'})
 
-                dispatch({
-                    type: "BerhasilLogin",
-                    payload: {id, username}
-                })
-
-                // Membuat sebuah file cookie dengan nama masihLogin, dan valuenya adalah username yg login
-                // path : "/" agar dapat diakses di setiap component
-                cookie.set('masihLogin', username, {path:'/'})
-
-            } else {
-               dispatch({
-                   type: 'ERROR',
-                   payload: "Username and Password don't match"
-               })
-
-                // setelah tiga detik akan kirim action untuk menghapus pesan error dan success
-                // ini akan menyebabkan component render ulang dan menghilangkan pesan error pada
-                // login dan register
-               
-            }
-
-        }).catch(err => {
-            console.log("System Error")
-        })
+            dispatch({
+                type: 'LOGIN_SUCCESS',
+                payload: {
+                    id: res.data._id , name: res.data.name
+                }
+            })
+            
+        } catch (e) {
+            console.log(e);
+            
+        }
     }
 }
 
 
-export const onRegisterUser = (user,email,pass) => {
+
+export const onRegister = (username, email, password) => {
     return dispatch => {
-        axios.get('http://localhost:1996/users', {
-            params: {
-                username: user
-            }
-        }).then(res => {
-            console.log(res.data)
-            if(res.data.length === 0){
-                axios.post('http://localhost:1996/users', {
-                    username: user,
-                    email: email,
-                    password: pass
-                }).then(res => {
-                    dispatch({
-                        type: 'AUTH_SUCCESS',
-                        payload: 'Register Succeeded'
-                    })
-
-                    setTimeout(() => {
-                        dispatch({
-                            type: 'AUTH_NO_MESS'
-                        })
-                    }, 3000);
-                })
-            } else {
-                dispatch({
-                    type: 'AUTH_ERROR',
-                    payload: 'Username has been taken'
-                })
-
-                setTimeout(() => {
-                    dispatch({
-                        type: 'AUTH_NO_MESS'
-                    })
-                }, 3000);
-            }
+        axios.post('/users', {
+            username, email, password
+        }). then (res => {
+            console.log("Register Success");
+        }).catch (e => {
+            console.log(e);
+            
         })
     }
 }
+
 
 export const onLogoutUser = () => {
     cookie.remove('masihLogin')
@@ -93,6 +50,7 @@ export const onLogoutUser = () => {
          type: 'Logout_User'
      }
 }
+
 
 export const keepLogin = (user) => {
     return dispatch => {
